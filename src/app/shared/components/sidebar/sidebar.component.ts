e51e14cb4e7a63bb77355ced87542fb76b0caf0d
anupamsoni <anupamsoni27@gmail.com>
2026-03-21 18:08:39 +0530

    feat: Implement 'Back to Home' link on login, make sidebar brand navigate to home, and add conditional 'Log In' link to sidebar.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ import { ThemeService } from '../../../core/services/theme.service';
     <header class="top-nav">
       <div class="nav-container">
         <!-- Logo/Brand -->
-        <div class="nav-brand">
+        <div class="nav-brand" [routerLink]="['/']" style="cursor: pointer;">
           <div class="brand-icon">S</div>
           <span class="brand-name">StockDash</span>
         </div>
@@ -18,7 +18,7 @@ import { ThemeService } from '../../../core/services/theme.service';
         <nav class="nav-menu">
           <a class="nav-item" [routerLink]="['/dashboard']" routerLinkActive="active">
             <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
             </svg>
             <span>Dashboard</span>
           </a>
@@ -28,12 +28,12 @@ import { ThemeService } from '../../../core/services/theme.service';
             </svg>
             <span>Stocks</span>
           </a>
-          <a class="nav-item" [routerLink]="['/funds']" routerLinkActive="active">
+          <!-- <a class="nav-item" [routerLink]="['/funds']" routerLinkActive="active">
             <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <span>Funds</span>
-          </a>
+          </a> -->
           <a class="nav-item" [routerLink]="['/how-it-works']" routerLinkActive="active">
             <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -55,6 +55,13 @@ import { ThemeService } from '../../../core/services/theme.service';
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
             </svg>
           </button>
+
+          <a *ngIf="!userProfile" class="nav-item" [routerLink]="['/login']" routerLinkActive="active">
+            <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+            </svg>
+            <span>Log In</span>
+          </a>
 
           <a *ngIf="userProfile" class="nav-item" [routerLink]="['/profile']" routerLinkActive="active">
             <img *ngIf="userProfile.picture" [src]="userProfile.picture" [alt]="userProfile.name" class="user-avatar">
